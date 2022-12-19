47bdc0e21d0bce205afabecfe8472ff9597ac5e9
willen <willen.sesantos@gmail.com>
2022-12-19 20:25:32 -0300

    docs: Documenta criação de usuário com swagger

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import swaggerUI from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
 import "dotenv/config";
 import { router } from "./routes";
 
@@ -6,7 +8,8 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
-app.use(router);
+app.use(router); //endpoints
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument)); //documentação com swagger
 
 app.listen(PORT, () => {
   console.log(`Server road on port ${PORT}`);
